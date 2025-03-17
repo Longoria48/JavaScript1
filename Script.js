e5267d32b63a4a778b1dfeaaee9a973c9e12e4c6
Longoria48 <longoriaeagles36@yahoo.com>
2025-03-17 17:05:11 -0500

    anything

---
+++ Script.js
@@ -0,0 +1,12 @@
+    //display alert good afternoon
+    window.alert("Good afternoon");
+    //declare the variable to hold the div element
+    let container = document.getElementById("container");
+    //getElementById is the method to locate the element by the ID
+    //container us an HTML element, it is a div in this case
+    //set the content of element
+    container.innerHTML = "This is the container place";
+    //Change the color style to red
+    container.style.color = "red";
+    //add the new element p into the page
+    document.write("<p>This is new paragraph</p>");
